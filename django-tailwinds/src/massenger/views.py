7c4fee0a131ce29dfddfc7acccf0cbab33436c09
root <root@Attito>
2023-11-16 09:22:06 +0000

    add front message

--- django-tailwinds/src/massenger/views.py
+++ django-tailwinds/src/massenger/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def first_page(request):
-    return render(request, 'first_page.html')
+    return render(request, 'messenger/first_page.html')
